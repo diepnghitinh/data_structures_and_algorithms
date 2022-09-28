f47bc82cae69b7674422851e958068ceb6f3bfd6
nguyen phuc nguyen <>
2022-09-28 22:24:45 +0700

    update design pattern

---
+++ dp__creational__factory_method.go
@@ -0,0 +1,94 @@
+package main
+
+import "fmt"
+
+/*
+
+Factory Method là mẫu thiết kế qua kế thừa dựa vào một lớp con để xử lý việc khởi tạo đối tượng mong muốn.
+
+Output:
+
+*/
+
+type IGun interface {
+	setName(name string)
+	setPower(power int)
+	getName() string
+	getPower() int
+}
+
+// Gun class
+type Gun struct {
+	name  string
+	power int
+}
+
+func (g *Gun) setName(name string) {
+	g.name = name
+}
+
+func (g *Gun) getName() string {
+	return g.name
+}
+
+func (g *Gun) setPower(power int) {
+	g.power = power
+}
+
+func (g *Gun) getPower() int {
+	return g.power
+}
+
+// AK47
+type Ak47 struct {
+	Gun
+}
+
+// Mấu chốt factory method nằm ở đây
+func newAk47() IGun {
+	return &Ak47{
+		Gun: Gun{
+			name:  "AK47 gun",
+			power: 4,
+		},
+	}
+}
+
+// Musket
+type musket struct {
+	Gun
+}
+
+func newMusket() IGun {
+	return &musket{
+		Gun: Gun{
+			name:  "Musket gun",
+			power: 1,
+		},
+	}
+}
+
+func getGun(gunType string) (IGun, error) {
+	if gunType == "ak47" {
+		return newAk47(), nil
+	}
+	if gunType == "musket" {
+		return newMusket(), nil
+	}
+	return nil, fmt.Errorf("Wrong gun type passed")
+}
+
+func dp__creational__factory_method() {
+	ak47, _ := getGun("ak47")
+	musket, _ := getGun("musket")
+
+	printDetails(ak47)
+	printDetails(musket)
+}
+
+func printDetails(g IGun) {
+	fmt.Printf("Gun: %s", g.getName())
+	fmt.Println()
+	fmt.Printf("Power: %d", g.getPower())
+	fmt.Println()
+}
